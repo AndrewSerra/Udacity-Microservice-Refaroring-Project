e5b5c95d2f199247baa7b22a2e27f74bd0ad2dc1
Andrew Serra <acs8929@rit.edu>
2021-04-14 19:26:54 -0400

    separated user and feed api

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=adminudagram
+export POSTGRES_PASSWORD=adminudagram
+export POSTGRES_HOST=udagramfinalproject.cye6s9hsrwkm.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagramfinalproject
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=helloworld
 export URL=http://localhost:8100
