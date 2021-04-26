c5293df09382b3b3692d4e0b47227c317cde8a62
Andrew Serra <acs8929@rit.edu>
2021-04-26 18:32:12 -0400

    got the reverse proxy external ip and set it in frontend file

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2201574619664dc7b53e1f067f85422-1848973135.us-east-2.elb.amazonaws.com'
 };
 
 /*
