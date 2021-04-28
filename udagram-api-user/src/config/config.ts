0daa49056c0e447f43003b074038de4e76db787d
Andrew Serra <acs8929@rit.edu>
2021-04-27 22:10:11 -0400

    fixed config issue with media bucket

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_media_bucket': process.env.AWS_MEDIA_BUCKET,
   'url': process.env.URL,
   'jwt': {
     'secret': process.env.JWT_SECRET,
